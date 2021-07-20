367a379b911573c347b9feee256d67dcc6475668
Brand Fang <brandon-cn@outlook.com>
2021-07-20 11:44:48 +0800

    Update api.js

--- modules/api.js
+++ modules/api.js
@@ -3,7 +3,7 @@ var request = require('request');
 var __api = {
     get_question_rand() {
         return new Promise(function(Resolved){
-          return request('https://miniprogram.onesmart.org/api/question/get_pk_rand', (error, response, body) =>{
+          return request('https://***/api/question/get_pk_rand', (error, response, body) =>{
             Resolved(body);
           });
         });
